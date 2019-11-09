8ba85cb2d5b5d2f9359d9a3047c1166b6603e998
Duncan Leo <hello@duncanleo.me>
2019-11-09 17:09:29 +0800

    add ArduinoOTA + fix return status codes

--- combo/balcony/balcony.ino
+++ combo/balcony/balcony.ino
@@ -6,6 +6,8 @@
 #include <ESP8266WiFiMulti.h> 
 #include <ESP8266mDNS.h>
 #include <ESP8266WebServer.h>
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
 
 Pmsx003 pms(4, 5);
 
@@ -59,10 +61,45 @@ void setup() {
   server.on("/sleep", handleSleep);
   server.on("/dht22", handleDHT22);
 
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_SPIFFS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
+
   server.begin();
 }
 
 void loop() {
+  if (WiFi.status() != WL_CONNECTED) return;
+  ArduinoOTA.handle();
   server.handleClient();
 }
 
@@ -71,12 +108,12 @@ auto lastRead = millis();
 void handleWake() {
   pms.write(Pmsx003::cmdWakeup);
   pms.waitForData(Pmsx003::wakeupTime);
-  server.send(404, "application/json", "{\"status\": true, \"message\": \"device woken\"}");
+  server.send(200, "application/json", "{\"status\": true, \"message\": \"device woken\"}");
 }
 
 void handleSleep() {
   pms.write(Pmsx003::cmdSleep);
-  server.send(404, "application/json", "{\"status\": true, \"message\": \"device slept\"}");
+  server.send(200, "application/json", "{\"status\": true, \"message\": \"device slept\"}");
 }
 
 void handleRead() {
