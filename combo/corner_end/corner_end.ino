33d1b2a00741ac5cc53f5d6c75f797ba5532c1ee
Duncan Leo <hello@duncanleo.me>
2019-12-29 11:10:16 +0800

    chore: replace PubSubClient with arduino-mqtt

--- combo/corner_end/corner_end.ino
+++ combo/corner_end/corner_end.ino
@@ -1,21 +1,26 @@
 #include <ESP8266WiFi.h>
-#include <PubSubClient.h>
 #include <Wire.h>
 #include <Adafruit_Sensor.h>
 #include <Adafruit_BME280.h>
+#include <ArduinoOTA.h>
+#include <MQTT.h>
 
 #define SEALEVELPRESSURE_HPA (1013.25)
+#define HOSTNAME "esp-corner-end"
 
 Adafruit_BME280 bme;
 
 WiFiClient wifiClient;
-PubSubClient client(wifiClient);
+MQTTClient mqttClient;
 
 int rainPin = A0;
 int thresholdValue = 500;
 
+const char* kSsid = "";
+const char* kPassword = "";
+
 void setup() {
-  Serial.begin(9600);
+  Serial.begin(115200);
   
   // setup Rain sensor
   pinMode(rainPin, INPUT);
@@ -27,7 +32,9 @@ void setup() {
     while (1);
   }
 
-  WiFi.begin("", "");
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(kSsid, kPassword);
+  WiFi.hostname(HOSTNAME);
 
   Serial.print("Connecting to WiFi..");
   
@@ -36,39 +43,71 @@ void setup() {
     Serial.print(".");
   }
 
-  Serial.println("Connected to the WiFi network");
+  Serial.print("Connected to ");
+  Serial.println(kSsid);
+  Serial.print("IP address: ");
+  Serial.println(WiFi.localIP().toString());
 
-  client.setServer("192.168.1.10", 1883);
+  mqttClient.begin("192.168.1.10", wifiClient);
+  mqttConnect();
 
-  mqtt();
+  ArduinoOTA.setHostname(HOSTNAME);
+
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_SPIFFS
+      type = "filesystem";
+    }
+
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    Serial.println("Start updating " + type);
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nEnd");
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("Error[%u]: ", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
+
+  Serial.println("ArduinoOTA ready");
 }
 
-void mqtt() {
-  while (!client.connected()) {
-    Serial.println("Connecting to MQTT...");
-
-    if (client.connect("ESP8266Client")) {
-      Serial.println("connected");
-    } else {
-      Serial.print("failed with state ");
-      Serial.print(client.state());
-      delay(2000);
-    }
+void mqttConnect() {
+  Serial.println("Connecting to MQTT...");
+  while (!mqttClient.connect(HOSTNAME)) {
+    Serial.print(".");
+    delay(1000);
   }
 }
 
 unsigned long prevMillis = 0;
-bool singleForcePublish = true;
 bool isWet = false;
 
 void loop() {
-  // put your main code here, to run repeatedly:
+  if (WiFi.status() != WL_CONNECTED) return;
+  mqttClient.loop();
+  delay(10);
+  ArduinoOTA.handle();
 
-  bool connected = client.loop();
-
-  if (!connected) {
-    mqtt();
-    singleForcePublish = true;
+  if (!mqttClient.connected()) {
+    mqttConnect();
   }
 
   int sensorValue = analogRead(rainPin);
@@ -76,10 +115,10 @@ void loop() {
   unsigned long currentMillis = millis();
   bool wet = sensorValue < thresholdValue;
 
-  if (wet != isWet || singleForcePublish) {
-    char sbuf[150];
-    sprintf(sbuf, "{\"wet\": %s }", wet ? "true" : "false");
-    client.publish("corner-end/rain", sbuf);
+  if (wet != isWet || prevMillis == 0) {
+    char wetData[150];
+    sprintf(wetData, "{\"wet\": %s }", wet ? "true" : "false");
+    mqttClient.publish("corner-end/rain", wetData);
   }
 
   if (currentMillis - prevMillis >= 2000) {
@@ -89,14 +128,18 @@ void loop() {
     Serial.print(wet);
     isWet = wet;
 
+    // ESP Status
+    char espStatus[150];
+    sprintf(espStatus, "{\"uptime\": %d, \"rssi\": %d}", millis(), WiFi.RSSI());
+
+    mqttClient.publish("corner-end/status", espStatus);
+
     Serial.print(" Temperature = ");
     Serial.print(bme.readTemperature());
     Serial.println(" *C");
 
-    char sbuf[150];
-    sprintf(sbuf, "{\"temp_c\": %f, \"pressure\": %f, \"altitude\": %f, \"humidity\": %f }", bme.readTemperature(), bme.readPressure() / 100.0F, bme.readAltitude(SEALEVELPRESSURE_HPA), bme.readHumidity());
-    client.publish("corner-end/bme280", sbuf);
+    char bmeData[150];
+    sprintf(bmeData, "{\"temp_c\": %f, \"pressure\": %f, \"altitude\": %f, \"humidity\": %f }", bme.readTemperature(), bme.readPressure() / 100.0F, bme.readAltitude(SEALEVELPRESSURE_HPA), bme.readHumidity());
+    mqttClient.publish("corner-end/bme280", bmeData);
   }
-  
-  singleForcePublish = false;
 }
